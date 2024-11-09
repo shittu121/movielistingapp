29fa814b2b2061834e9c5c574bbaca5746340954
shittu121 <shittufaruq6@gmail.com>
2024-11-09 17:22:48 +0000

    Fixed /movies page

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import MoviesList from '../components/movies'
+import React, { Suspense } from 'react';
+import MoviesList from '../components/movies';
+
 export default function page() {
     return (
         <main className='md:px-16 px-4 mt-10'>
@@ -20,7 +21,7 @@ export default function page() {
                         <div className="relative">
                             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                                 <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                    <path stroke="currentColor" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                 </svg>
                             </div>
                             <input type="search" id="default-search" className="block w-full p-4 ps-10 text-xs text-gray-900 border border-gray-500 outline-none rounded-lg bg-transparent focus:ring-blue-500 focus:border-blue-500  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Movies ..." required />
@@ -30,8 +31,11 @@ export default function page() {
                 </div>
             </div>
 
-            <MoviesList />
+            {/* Wrap MoviesList in Suspense */}
+            <Suspense fallback={<div>Loading movies...</div>}>
+                <MoviesList />
+            </Suspense>
 
         </main>
-    )
+    );
 }
